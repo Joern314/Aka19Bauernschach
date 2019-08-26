9d02ed7346cb5f3d4ec80cb6fb9d803c2e4d777d
J.Stoehler <J.Stoehler@campus.lmu.de>
2019-08-26 15:20:56 +0200

    save

--- Evaluator.py
+++ Evaluator.py
@@ -3,15 +3,17 @@ class Evaluator(Object):
     def __init__(self):
         pass
     
-    def evaluate(knoten, alpha, beta):
-        """liefert Bewertung, optimalen Move"""
-        legalMoves = knoten.listLegalMoves()
-        m = -alpha
+    def evaluate(self, knoten, alpha, beta):
+        # liefert Bewertung, optimalen Move
+        # bricht ab wenn bewertung <= alpha oder >=beta
+
+        legalMoves = knoten.listLegalMoves() #liste aller kanten zu kindern
+        m = -alpha #
         bestmove = None
         for move in legalMoves:
             child = knoten.copy()
             child.applyMove(move)
-            e,_ = evaluate(child, -beta, m)
+            e,_ = self.evaluate(child, -beta, m)
             if e < m:
                 m = e
                 bestmove = move
