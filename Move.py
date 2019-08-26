93a5dcb26ee9da2d8175e93bf8458bc8cf89ec17
Jana Lemke <jana.lemke@rwth-aachen.de>
2019-08-26 16:03:06 +0200

    Extended Move

--- Move.py
+++ Move.py
@@ -1,11 +1,20 @@
 class Move:
-    def __init__(self, figur, richtung, passing = False: bool):
+    def __init__(self, figur, richtung, passing = False)
     	self.figur = figur
     	self.richtung = richtung
     	self.passing = passing
 
+    def is_passing(self):
+        return self.passing
+
+    def get_richtung(self):
+        return self.richtung
+
+    def get_figur(self):
+        return self.figur
+
 	@staticmethod	
-    def create_from_movecode(movecode: str):
+    def create_from_movecode(movecode)
     	if movecode = "pass":
             return Move(None, None, passsing = True)
         else:
