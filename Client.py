d6344d7c6de842d2db2bd41882c9bffc00a16b68
J.Stoehler <J.Stoehler@campus.lmu.de>
2019-08-26 16:00:53 +0200

    sth client

--- Client.py
+++ Client.py
@@ -1,7 +1,29 @@
-class Client:
-    pass
+import math
+import Evaluator
+import GameState
+import Move
 
+class Client:
+    def __init__(self):
+        self.black_or_white = None
+        self.innerstate = GameState()
+        self.evaluator = Evaluator()
+        pass
+
+    def parseMove(self, string):
+        pass
+    
+    def writeMove(self, string):
+        pass
+    
+    def findBestMove(self):
+        rating, move = self.evaluator.evaluate(self.innerstate, -math.inf, math.inf)
+        return move
+    
+    def run(self):
+        
 
 if __name__ == "__main__":
     # main function
-    pass
+    client = Client()
+    client.run()
