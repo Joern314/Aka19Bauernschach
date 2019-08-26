baa4d1ff64fdfef2c70286c3e3f2d95e769f037f
J.Stoehler <J.Stoehler@campus.lmu.de>
2019-08-26 15:45:58 +0200

    save

--- Evaluator.py
+++ Evaluator.py
@@ -1,20 +1,24 @@
-
-class Evaluator(Object):
+class Evaluator:
     def __init__(self):
         pass
     
     def evaluate(self, knoten, alpha, beta):
-        # liefert Bewertung
+        # Annahme: beta > alpha
+        # liefert Bewertung + besten Zug
         # bricht ab wenn bewertung <= alpha oder >=beta
 
-        legalMoves = knoten.listLegalMoves() #liste aller kanten zu kindern
-        m = -alpha  #unsere beste Wahl. Schlechter als alpha ist uninteressant
+        legalMoves = knoten.listAllLegalMoves() #liste aller kanten zu kindern
+        m = -alpha #schlechteste Stellung für den Gegner hat Wert m
+        bestmove = None
         for move in legalMoves:
             child = knoten.copy()
             child.applyMove(move)
-            e = self.evaluate(child, -beta, m)  #bessere züge als m sind nicht interessant weil Gegner zieht
-            m = min(m,e)
+            e, _ = self.evaluate(child, -beta, m)  # <= m, >=-beta
+            if m < e: # neue schlechteste Stellung für Gegner
+                bestmove = move
+                m = e
+                
             if m <= -beta:
                 break
-        return -m
+        return -m, bestmove
     
