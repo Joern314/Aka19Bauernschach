b5508c478a8f0e042087880dfec8681a5018037d
J.Stoehler <J.Stoehler@campus.lmu.de>
2019-08-31 10:27:06 +0200

    Bits

--- Bewertung.py
+++ Bewertung.py
@@ -1,4 +1,4 @@
-
+from GameState import GameState
 
 def bewerte_unentschieden(board, is_white): # bewerte aus sicht des aktuellen spielers
     return 0
@@ -9,7 +9,15 @@ def bewerte_figurzahl(board, is_white):
     
     return (len(pawns_white) - len(pawns_black))/float(board.size_x) * (+1 if is_white else -1)
 
+def bewerte_moves(board: GameState, is_white):
+    if is_white:
+        moved_b = (board.black | (board.black >> 1)) & ~(board.white)
+        kill_wr = moved_b & (board.white << (8+1))
+        kill_wl = moved_b & (board.white >> (8-1))
+        
+
 estimate_functions = {
     "unentschieden": bewerte_unentschieden,
-    "figurzahl": bewerte_figurzahl
+    "figurzahl": bewerte_figurzahl,
+    "moves": bewerte_moves
 }
---
+++ Bits.py
@@ -0,0 +1,12 @@
+import math
+
+def get_x(pawn):
+    return int(math.log2(pawn) / 8)
+
+def get_y(pawn):
+     return int(math.log2(pawn))%8
+
+def popcount32(x):
+    
+def popcount(x):
+    
